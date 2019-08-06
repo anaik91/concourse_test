858a077edb9197f9ff4f5354096859fe00be3906
I501950 <ashwin.kumar.naik@sap.com>
2019-08-06 08:47:04 +0000

    Added sample2.py

---
+++ sample2.py
@@ -0,0 +1,14 @@
+#/usr/local/bin/python
+import time
+import sys
+import os
+
+def main():
+    print('\n')
+    print('************************** Start Job **************************\n')
+    print('Time is : {}'.format(time.time()))
+    print('Python Version is : {}\n'.format(sys.version))
+    print('**************************  End Job   *************************')
+
+if __name__ == '__main__':
+    main()
