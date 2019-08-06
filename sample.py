2449c63a0be75a530d3cb0c24dcdde06ab8d77b3
anaik91 <ashwinkumarnaik91@gmail.com>
2019-08-06 11:23:18 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,12 @@
+import time
+import sys
+
+def main():
+    print('\n')
+    print('************************** Start Job **************************\n')
+    print('Time is : {}'.format(time.time()))
+    print('Python Version is : {}\n'.format(sys.version))
+    print('**************************  End Job   *************************')
+
+if __name__ == '__main__':
+    main()
