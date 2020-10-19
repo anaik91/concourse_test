71d2fe0e8b6128ee9cf8e698cba67a6ff216156a
anaik91 <ashwinkumarnaik91@gmail.com>
2020-10-19 12:20:32 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,12 +3,20 @@ import time
 import sys
 import os
 
+def banner(comment):
+    size = int(os.popen('tput cols').read().strip())
+    print('\n')
+    print('#' * size)
+    print(comment.center(size))
+    print('#' * size)
+    
 def main():
     print('\n')
     print('************************** Start Job **************************\n')
     print('Time is : {}'.format(time.time()))
     print('Python Version is : {}\n'.format(sys.version))
     print('**************************  End Job   *************************')
+    banner('TESTING')
     try:
         os.mkdir('out')
     except FileExistsError as e:
