2e00ebb6be5102d120376e3231092a84f81048b6
anaik91 <ashwin.kumar.naik@sap.com>
2021-03-02 11:36:03 +0530

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -9,7 +9,7 @@ def main():
     print('Time is : {}'.format(time.time()))
     print('Python Version is : {}\n'.format(sys.version))
     print('OPS Demo in Progress ..........')
-    print('Value for DEMO_KEY : {}'.format(os.getenv('DEMO_KEY'))
+    print('Value for DEMO_KEY : {}'.format(os.getenv('DEMO_KEY')))
     print('**************************  End Job   *************************')
 
 if __name__ == '__main__':
